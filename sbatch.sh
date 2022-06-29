45ba41057e721df5e9c62814bf7882415ed86249
Roberta Hunt <rehunt@ualberta.ca>
2022-06-29 14:36:21 +0200

    next

--- sbatch.sh
+++ sbatch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=Rove
 #number of independent tasks we are going to start in this script
-#SBATCH --mem-per-gpu=10000
+#SBATCH --ntasks=1 --cpus-per-task=4 --mem=10000M
 #We expect that our program should not run longer than 2 days
 #Note that a program will be killed once it exceeds this time!
 #SBATCH --time=3-00:00:00
 #gpu specs
-#SBATCH --gres=gpu:titanx:2
+#SBATCH -p gpu --gres=gpu:titanx:1
 #Skipping many options! see man sbatch
 # From here on, we can start our program
 
