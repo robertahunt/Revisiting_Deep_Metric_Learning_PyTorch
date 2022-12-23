d4575c21dd2b3e81d78ee64a9094911252b6aa20
Roberta Hunt <rehunt@ualberta.ca>
2022-12-23 13:03:46 +0100

    next

--- sbatch.sh
+++ sbatch.sh
@@ -17,11 +17,22 @@ python -m pip install -r /home/ngw861/01_abbey_rove/requirements.txt -f https://
 export GIT_PYTHON_REFRESH=quiet
 
 ##### RoveGenus #####
-python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim0 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=0  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
-python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim1 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=1  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
-python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim2 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=2  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
-python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim3 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=3  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
-python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim4 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=4  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
+
+###  TRIPLET  ###
+python main.py --dataset=roveGenus --suffix=tripD0 --source_path=/home/ngw861/01_abbey_rove/data --seed=0  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2  --loss=triplet --batch_mining=distance --arch=resnet50_frozen_normalize --augmentation=rove --use_tv_split
+python main.py --dataset=roveGenus --suffix=tripD1 --source_path=/home/ngw861/01_abbey_rove/data --seed=1  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2  --loss=triplet --batch_mining=distance --arch=resnet50_frozen_normalize --augmentation=rove --use_tv_split
+python main.py --dataset=roveGenus --suffix=tripD2 --source_path=/home/ngw861/01_abbey_rove/data --seed=2  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2  --loss=triplet --batch_mining=distance --arch=resnet50_frozen_normalize --augmentation=rove --use_tv_split
+python main.py --dataset=roveGenus --suffix=tripD3 --source_path=/home/ngw861/01_abbey_rove/data --seed=3  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2  --loss=triplet --batch_mining=distance --arch=resnet50_frozen_normalize --augmentation=rove --use_tv_split
+python main.py --dataset=roveGenus --suffix=tripD4 --source_path=/home/ngw861/01_abbey_rove/data --seed=4  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2  --loss=triplet --batch_mining=distance --arch=resnet50_frozen_normalize --augmentation=rove --use_tv_split
+
+
+
+### MULTISIM
+#python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim0 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=0  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
+#python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim1 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=1  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
+#python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim2 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=2  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
+#python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim3 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=3  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
+#python main.py --dataset=roveGenus  --augmentation=rove --suffix=multisim4 --use_tv_split --source_path=/home/ngw861/01_abbey_rove/data --seed=4  --bs=8 --gradient_accumulation_steps=14 --samples_per_class=2 --loss=multisimilarity --arch=resnet50_frozen_normalize
 
 ##### CUB200 BS 112 #####
 
